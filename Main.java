2a8cd690a74e6e2ba6193182d24d4db47eccd35c
Sevia_6706174030 <48724549+seviamega4030@users.noreply.github.com>
2019-04-06 10:43:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+    	 Scanner s = new Scanner(System.in);
+        int n;
+
+        System.out.print("Input : ");
+        n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int x = cari(i);
+
+            System.out.print(x + " ");
+        }
+
+    }
+
+    public static int cari(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return cari(n - 1) + cari(n - 2);
+        }
+    }
+}
